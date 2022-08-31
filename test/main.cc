76a8706247f5e74cf021115238ba16e9c0962e40
Jeonghoon <acoustically1011@gmail.com>
2022-08-31 23:45:48 +0900

    add google test

--- test/main.cc
+++ test/main.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 
+#include "gtest/gtest.h"
 #include "chttpp/chttpp.hh"
 
 
 int main(int argc, char** argv) {
-  HelloWorld();
-  return 0;
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
